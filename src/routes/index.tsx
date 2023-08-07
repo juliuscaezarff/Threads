b0ceb543c93d4cc0aa1abd3653dda371f1b019b5
julius caezar <julius.caezar25@gmail.com>
2023-08-06 23:47:48 -0400

    feat: importando as fonts dos projetos e criando o fluxo de carregamento das fonts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,53 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import TabRoutes from './tab.routes'
 
+import * as SplashScreen from 'expo-splash-screen'
+
+import {
+  useFonts,
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+
 const Routes = () => {
+  let [fontsLoaded] = useFonts({
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black,
+  });
+
+  React.useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync()
+    }
+
+    prepare()
+  }, [])
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if(!fontsLoaded) return null;
+
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={onLayoutRootView}>
       <TabRoutes />
     </NavigationContainer>
   )
