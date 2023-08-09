2be7adb4fd39846635b7038047fa2abc9e2418f4
julius caezar <julius.caezar25@gmail.com>
2023-08-09 14:48:40 -0400

    feat: criando estilos iniciais para a home

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+import { dark } from '../../themes/dark';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${dark.colors.background};
+`;
