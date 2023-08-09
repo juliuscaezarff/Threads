b455a7b8889caeb402016a94209ea7a59352b7f1
julius caezar <julius.caezar25@gmail.com>
2023-08-09 15:49:13 -0400

    feat: Criando os componentes estilizados de AvatarGroup

--- src/components/CardUser/index.tsx
+++ src/components/CardUser/index.tsx
@@ -2,24 +2,42 @@ import React from 'react'
 
 import * as S from './styles'
 
+const UserInfo = () => (
+  <S.Column>
+    <S.Name>Julius Caezar</S.Name>
+    <S.UserName>juliusCaezar</S.UserName>
+    <S.RowUserName>
+      <S.TagContainer>
+        <S.Tag>threads.net</S.Tag>
+      </S.TagContainer>
+    </S.RowUserName>
+  </S.Column>
+)
+
 const CardUser = () => {
   return (
     <S.Container>
       <S.Row>
-        <S.Column>
-          <S.Name>Julius Caezar</S.Name>
-          <S.UserName>juliusCaezar</S.UserName>
-          <S.RowUserName>
-            <S.TagContainer>
-              <S.Tag>threads.net</S.Tag>
-            </S.TagContainer>
-          </S.RowUserName>
-        </S.Column>
+        <UserInfo />
         <S.Avatar
           source={{ uri: 'https://randomuser.me/api/portraits/men/81.jpg' }}
         />
       </S.Row>
       <S.Bio>Software Engineer | Never stop learning</S.Bio>
+      <S.AvatarGroupContainer>
+        <S.AvatarGroup
+          source={{ uri: 'https://randomuser.me/api/portraits/men/81.jpg' }}
+        />
+        <S.AvatarGroup
+          ml="-8"
+          source={{ uri: 'https://randomuser.me/api/portraits/men/81.jpg' }}
+        />
+        <S.AvatarGroup
+          ml="-8"
+          source={{ uri: 'https://randomuser.me/api/portraits/men/81.jpg' }}
+        />
+        <S.Followers>2957 seguidores</S.Followers>
+      </S.AvatarGroupContainer>
     </S.Container>
   )
 }
