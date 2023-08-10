183e07d75f330183289af091af2df35893be2c8e
julius caezar <julius.caezar25@gmail.com>
2023-08-10 11:22:21 -0400

    feat: criando a estilização para nome e post do usuário

---
+++ src/components/Threads/styles.ts
@@ -0,0 +1,22 @@
+import { styled } from "styled-components/native";
+import { dark } from "../../themes/dark";
+
+export const Container = styled.View``
+
+export const ContainerPost = styled.TouchableOpacity`
+  
+`
+
+export const UserName = styled.Text`
+  font-family: "Inter_500Medium";
+  font-size: 14px;
+  color: ${dark.colors.white};
+`
+
+export const Post = styled.Text`
+  font-family: "Inter_400Regular";
+  font-size: 14px;
+  margin-top: 8px;
+  margin-right: 48px;
+  color: ${dark.colors.white};
+`
