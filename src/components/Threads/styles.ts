9b1a71400a53948a1bee76c4666052a05a032087
julius caezar <julius.caezar25@gmail.com>
2023-08-10 12:02:46 -0400

    feat; criando a estilização do componente de ActionsButtons

--- src/components/Threads/styles.ts
+++ src/components/Threads/styles.ts
@@ -1,11 +1,9 @@
-import { styled } from "styled-components/native";
-import { dark } from "../../themes/dark";
+import { styled } from 'styled-components/native'
+import { dark } from '../../themes/dark'
 
 export const Container = styled.View``
 
-export const ContainerPost = styled.TouchableOpacity`
-  
-`
+export const ContainerPost = styled.TouchableOpacity``
 
 export const UserName = styled.Text`
   font-family: "Inter_500Medium";
@@ -14,9 +12,20 @@ export const UserName = styled.Text`
 `
 
 export const Post = styled.Text`
-  font-family: "Inter_400Regular";
+  margin-bottom: 16px;
+  font-family: 'Inter_400Regular';
   font-size: 14px;
   margin-top: 8px;
-  margin-right: 48px;
   color: ${dark.colors.white};
 `
+
+export const ActionsContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Action = styled.TouchableOpacity`
+  margin-right: 16px;
+`
+
+export const Icon = styled.Image``
