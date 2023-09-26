326749a4365f897ae43056c6fa3962f1aca75673
Julius Caezar <julius.caezar25@gmail.com>
2023-09-26 20:58:25 -0300

    feat: criando estilos para a página home

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,7 +1,9 @@
-import styled from 'styled-components/native';
-import { dark } from '../../themes/dark';
+import styled from 'styled-components/native'
+import { dark } from '../../themes/dark'
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${dark.colors.background};
-`;
+`
+
+export const List = styled.FlatList``;
