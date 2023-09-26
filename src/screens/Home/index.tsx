f42c43a31cc091dfc31d6c41f4f9b253c9807ac3
Julius Caezar <julius.caezar25@gmail.com>
2023-09-26 20:57:54 -0300

    feat: criando a página home com os posts

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from 'react'
 
 import * as S from './styles'
 
+import posts from '../../data/posts.json'
+import Threads from '../../components/Threads'
+import { ListRenderItemInfo } from 'react-native'
+import { IPost } from '../../components/Threads/types'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
 const Home = () => {
-  return <S.Container />;
+  return (
+    <S.Container>
+      <SafeAreaView>
+        <S.List
+          data={posts}
+          renderItem={({ item }: ListRenderItemInfo<IPost>) => (
+            <Threads {...item} />
+          )}
+        />
+      </SafeAreaView>
+    </S.Container>
+  )
 }
 
-export default Home;
+export default Home
