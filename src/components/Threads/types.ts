e2bdb46b1ca2be027f81576b9e2e30ffdcf5aaf9
Julius Caezar <julius.caezar25@gmail.com>
2023-09-26 20:53:31 -0300

    feat: tipando os dados que vem do json

--- src/components/Threads/types.ts
+++ src/components/Threads/types.ts
@@ -1,5 +1,25 @@
 export interface IAvatarGroupStyle {
-  w?: string;
-  h?: string;
-  top?: string;
+  w?: string
+  h?: string
+  top?: string
+}
+
+export interface IPost {
+  id: number
+  name: string
+  username: string
+  avatar_uri: string
+  post?: string
+  postImage?: string
+  likes: string
+  answers: IAnswers[]
+}
+
+export interface IAnswers {
+  id: number
+  name: string
+  username: string
+  avatar_uri: string
+  likes: string
+  answers: string
 }
